3e8f8e9adfbbaa558ad4d1b5d025809f8b87e787
liupeng <laupeng1989@gmail.com>
2024-03-12 11:22:25 +0800

    add dit model.

--- rfwave/mm_dit.py
+++ rfwave/mm_dit.py
@@ -1,6 +1,8 @@
 import collections
 import torch
 import torch.nn.functional as F
+import math
+import numpy as np
 
 from itertools import repeat
 from functools import partial
@@ -20,8 +22,44 @@ to_1tuple = _ntuple(1)
 to_2tuple = _ntuple(2)
 
 
+def get_1d_sincos_pos_embed(embed_dim, max_sqe_len):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    pos = np.arange(max_sqe_len)
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float64)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out)  # (M, D/2)
+    emb_cos = np.cos(out)  # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb.astype(np.float32)
+
+
 def modulate(x, shift, scale):
     return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
+
+
+def sequence_mask(length, max_length=None):
+    if max_length is None:
+        max_length = length.max()
+    x = torch.arange(max_length, dtype=length.dtype, device=length.device)
+    return x.unsqueeze(0) < length.unsqueeze(1)
+
+
+def score_mask(length, max_length=None):
+    seq_mask = sequence_mask(length, max_length)
+    sco_mask = torch.zeros_like(seq_mask, dtype=torch.float)
+    sco_mask.masked_fill_(~seq_mask, float('-inf'))
+    return sco_mask.unsqueeze(1).unsqueeze(2)
 
 
 class RMSNorm(torch.nn.Module):
@@ -181,6 +219,163 @@ class MMDiTBlock(nn.Module):
         return x1, x2
 
 
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations.
+    """
+    def __init__(self, hidden_size, frequency_embedding_size=256, pe_scale=1000):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+        self.pe_scale = pe_scale
+
+    def timestep_embedding(self, t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
+        ).to(device=t.device)
+        args = self.pe_scale * t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+
+class FinalLayer(nn.Module):
+    """
+    The final layer of DiT.
+    """
+    def __init__(self, hidden_size, out_channels):
+        super().__init__()
+        self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
+        self.linear = nn.Linear(hidden_size, out_channels, bias=True)
+        self.adaLN_modulation = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(hidden_size, 2 * hidden_size, bias=True)
+        )
+
+    def forward(self, x, c):
+        shift, scale = self.adaLN_modulation(c).chunk(2, dim=1)
+        x = modulate(self.norm_final(x), shift, scale)
+        x = self.linear(x)
+        return x
+
+
+class MMDiT(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        hidden_size=768,
+        depth=8,
+        num_heads=16,
+        mlp_ratio=4.0,
+        max_seq_len=4096,
+        num_bands=8,
+        pe_scale=1000,
+    ):
+        super().__init__()
+
+        self.t_embed = TimestepEmbedder(hidden_size, pe_scale=pe_scale)
+        pos_embed = torch.from_numpy(get_1d_sincos_pos_embed(hidden_size, max_seq_len))
+        self.register_buffer("pos_embed", pos_embed, persistent=False)
+        self.band_embed = nn.Embedding(num_bands, hidden_size)
+
+        self.m1_proj = nn.Linear(in_channels, hidden_size)
+        self.m2_proj = nn.Linear(out_channels, hidden_size)
+        self.blocks = nn.ModuleList([
+            MMDiTBlock(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)
+        ])
+        self.final_layer = FinalLayer(hidden_size, out_channels)
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Initialize transformer layers:
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+
+        # Initialize timestep embedding MLP:
+        nn.init.normal_(self.t_embed.mlp[0].weight, std=0.02)
+        nn.init.normal_(self.t_embed.mlp[2].weight, std=0.02)
+
+        # Initialize band embedding:
+        nn.init.normal_(self.band_embed.weight, mean=0.0, std=0.02)
+
+        # Zero-out adaLN modulation layers in DiT blocks:
+        for block in self.blocks:
+            nn.init.constant_(block.m1_adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.m1_adaLN_modulation[-1].bias, 0)
+            nn.init.constant_(block.m2_adaLN_modulation[-1].weight, 0)
+            nn.init.constant_(block.m2_adaLN_modulation[-1].bias, 0)
+
+        # Zero-out output layers:
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].weight, 0)
+        nn.init.constant_(self.final_layer.adaLN_modulation[-1].bias, 0)
+        nn.init.constant_(self.final_layer.linear.weight, 0)
+        nn.init.constant_(self.final_layer.linear.bias, 0)
+
+    def get_pos_embed(self, start, length):
+        pos = start.unsqueeze(1) + torch.arange(length.max(), device=start.device).unsqueeze(0)
+        pe = self.pos_embed[pos]
+        return pe
+
+    def forward(self,
+                x1, x1_start, x1_len,
+                x2, x2_start, x2_len,
+                t, bandwidth_id,
+                ctx=None, ctx_start=None, ctx_len=None):
+        x1_sco_mask = score_mask(x1_len)
+        x1_pe = self.get_pos_embed(x1_start, x1_len)
+        x1 = self.m1_proj(x1) + x1_pe
+
+        if ctx is not None:
+            ctx_start = (torch.zeros([ctx.size(0)], dtype=torch.long, device=ctx.device)
+                         if ctx_start is None else ctx_start)
+            ctx_len = (torch.ones([ctx.size(0)], dtype=torch.long, device=ctx.device) * ctx.size(1)
+                       if ctx_len is None else ctx_len)
+            ctx_sco_mask = score_mask(ctx_len)
+            ctx_pe = self.get_pos_embed(ctx_start, ctx_len)
+            ctx = self.m1_proj(ctx) + ctx_pe
+            x1 = torch.cat([ctx, x1], dim=1)
+            x1_sco_mask = torch.cat([ctx_sco_mask, x1_sco_mask], dim=-1)
+
+        x2_sco_mask = score_mask(x2_len)
+        x2_pe = self.get_pos_embed(x2_start, x2_len)
+        x2 = self.m2_proj(x2) + x2_pe
+
+        te = self.t_embed(t)
+        be = self.band_embed(bandwidth_id)
+        c = te + be
+
+        for block in self.blocks:
+            x1, x2 = block(x1, x2, c, x1_sco_mask, x2_sco_mask)
+
+        x2 = self.final_layer(x2, c)
+        return x2
+
+
 if __name__ == '__main__':
     B = 2
     N1 = 8
@@ -196,3 +391,14 @@ if __name__ == '__main__':
     c = torch.randn([B, C])
     x1_out, x2_out = block(x1, x2, c, mask1.view(B, 1, 1, N1), mask2.view(B, 1, 1, N2))
     print(x1_out.shape, x2_out.shape, torch.all(x1_out.isfinite()), torch.all(x2_out.isfinite()))
+
+    x1_start = torch.tensor([9, 10], dtype=torch.long)
+    x1_len = torch.tensor([8, 7], dtype=torch.long)
+    x2_start = torch.tensor([2, 3])
+    x2_len = torch.tensor([4, 7])
+    ctx_seq = torch.randn([B, 10, C])
+    bandwidth_id = torch.tensor([0, 1], dtype=torch.long)
+    t = torch.tensor([0.1, 0.2], dtype=torch.float)
+    model = MMDiT(C, C)
+    out = model(x1, x1_start, x1_len, x2, x2_start, x2_len, t, bandwidth_id, ctx_seq)
+    print(out.shape, torch.all(out.isfinite()))
