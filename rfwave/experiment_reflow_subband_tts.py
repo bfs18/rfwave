d4accf675cffb583ed02bc65de10f9d5dae9bb56
liupeng <laupeng1989@gmail.com>
2024-04-01 07:58:06 +0800

    revert stft loss.

--- rfwave/experiment_reflow_subband_tts.py
+++ rfwave/experiment_reflow_subband_tts.py
@@ -41,7 +41,7 @@ class RectifiedFlow(nn.Module):
         self.wave = wave
         self.equalizer = wave
         self.stft_norm = not wave
-        self.stft_loss = wave
+        self.stft_loss = False
         self.phase_loss = False
         self.overlap_loss = True
         self.num_bands = num_bands
