0795ed4c99fe0c1ffceaac9dd2f9b1ef2986152d
liupeng <laupeng1989@gmail.com>
2024-05-25 14:46:15 +0800

    minor fix.

--- rfwave/dit.py
+++ rfwave/dit.py
@@ -400,12 +400,15 @@ class DiTRFE2ETTSMultiTaskBackbone(Backbone):
             align_ctx = (rand_attn  @ x_token.detach().transpose(1, 2)).transpose(-1, -2)
         elif self.standalone_align and not self.standalone_distill:
             assert not (standalone_attn is None and duration is None)
-            bin_attn = binarize_attention(standalone_attn, num_tokens, length)
+            attn = standalone_attn
+            if attn is not None:
+                bin_attn = binarize_attention(attn, num_tokens, length)
+                align_ctx = (attn.squeeze(1)  @ x_token.detach().transpose(1, 2)).transpose(-1, -2)
+            else:
+                bin_attn = align_ctx = None
             ctx = self.sa_align_block(z_t1, x_token, bin_attn, duration, mod_c=te)
             # align_ctx = ctx  # compute aux loss for the outer standalone alignment block
             ctx = self.cross_attn(ctx, x, z_freq_cis, ctx_freq_cis, z_mask, ctx_mask, mod_c=te)
-            attn = standalone_attn
-            align_ctx = (attn.squeeze(1)  @ x_token.detach().transpose(1, 2)).transpose(-1, -2) if attn is not None else None  # for inference
         else:
             ctx = self.cross_attn(z_t1, x, z_freq_cis, ctx_freq_cis, z_mask, ctx_mask, mod_c=te)
             align_ctx = ctx.transpose(-1, -2)  # not really
